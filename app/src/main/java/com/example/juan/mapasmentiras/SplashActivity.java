0ef1809552a22dd6a0513f31e42f223d0726a009
juan <juandiegoz1918@gmail.com>
2018-08-21 09:57:57 -0500

    Juan

---
+++ app/src/main/java/com/example/juan/mapasmentiras/SplashActivity.java
@@ -0,0 +1,31 @@
+package com.example.juan.mapasmentiras;
+
+import android.content.Intent;
+import android.os.CountDownTimer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        CountDownTimer tiempo;
+
+        tiempo=new CountDownTimer(3000,1000) {
+            @Override
+            public void onTick(long l) {
+
+            }
+
+            @Override
+            public void onFinish() {
+                Intent miIntent=new Intent(SplashActivity.this,MainActivity.class);
+                startActivity(miIntent);
+                finish();
+            }
+        };
+        tiempo.start();
+    }
+}
